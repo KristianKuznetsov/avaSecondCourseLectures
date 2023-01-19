a0f25f6b6a23c0a50ecf630f8282a2621f3e9c4b
Kristian Kuznetsov <103145576+KristianKuznetsov@users.noreply.github.com>
2023-01-19 21:54:26 +0300

    Update singltone.java

--- Singleton/singltone.java
+++ Singleton/singltone.java
@@ -1,1 +1,10 @@
-
+public class LazyInitializedSingleton {
+    private static LazyInitializedSingleton instance;
+        private LazyInitializedSingleton(){}
+        public static LazyInitializedSingleton getInstance(){ // #3
+        if(instance == null){		//если объект еще не создан
+            instance = new LazyInitializedSingleton();	//создать новый объект
+        }
+        return instance;		// вернуть ранее созданный объект
+    }
+}
